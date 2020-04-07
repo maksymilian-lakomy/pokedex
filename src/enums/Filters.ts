22a57694a7970fd9b9fcec91f7d3066fe19fdc4f
Maksymilian Łakomy <zommer128@gmail.com>
2020-04-07 19:26:03 +0200

    Change enum filter to array

--- src/enums/Filters.ts
+++ src/enums/Filters.ts
@@ -1,5 +1,16 @@
-export enum Filters {
-    Colors = 'color',
-    Habitats = 'habitat',
-    Shapes = 'shape'
+export interface Filter {
+    name: string;
+    api: string;
 }
+
+export const filters = new Array<Filter>();
+filters.push({
+    name: 'colors',
+    api: 'color'
+}, {
+    name: 'habitats',
+    api: 'habitat'
+}, {
+    name: 'shapes',
+    api: 'shape'
+});
