caf47e81beee08d63d47994905cdfa68d76e7948
Maksymilian Łakomy <zommer128@gmail.com>
2020-06-04 13:47:06 +0200

    Create Queries.ts

---
+++ src/classes/Queries.ts
@@ -0,0 +1,48 @@
+import { Dictionary } from 'vue-router/types/router';
+
+export class Queries {
+    private readonly _queries: Record<string, Array<string>> = {};
+
+    constructor(queries: Dictionary<string | (string | null)[]>) {
+        for (const query in queries) {
+            if (queries[query] === null)
+                continue;
+            this._queries[query] = new Array<string>();
+            if (queries[query] instanceof Array)
+                this._queries[query] = [...queries[query] as Array<string>];
+            else
+                this._queries[query] = [queries[query] as string];
+        }
+    }
+
+    public get queries(): Readonly<Record<string, Array<string>>> {
+        return this._queries;
+    }
+
+    public setQuery(query: string, value: string | Array<string>): void {
+        if (value.length === 0 && this._queries[query] !== undefined) {
+            delete this._queries[query];
+        } else {
+            this._queries[query] = typeof value === 'string' ? [value] : [...value];
+        }
+    }
+
+    public addToQuery(query: string, value: string | Array<string>) {
+        if (this._queries[query] === undefined)
+            this._queries[query] = [];
+        if (typeof value === 'string')
+            this._queries[query].push(value);
+        else
+            this._queries[query].push(...value);
+    }
+
+    public removeFromQuery(query: string, value: string | Array<string>) {
+        if (!this._queries[query]) return;
+        if (typeof value === 'string') 
+            this._queries[query] = this._queries[query].filter(_query => query !== value);
+        else 
+            this._queries[query] = this._queries[query].filter(_query => !value.includes(_query));
+        if (this._queries[query].length === 0)
+            delete this._queries[query];
+    }
+}
