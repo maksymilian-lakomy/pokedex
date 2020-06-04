c21df5fca1870255e146a2cf63abd64e37cfd674
Maksymilian Łakomy <zommer128@gmail.com>
2020-06-04 13:52:34 +0200

    Create new HeaderName component

---
+++ src/components/Header/HeaderName.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-link to="/">
+        <h1>Pokedex</h1>
+    </router-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class HeaderName extends Vue {
+    
+}
+</script>
